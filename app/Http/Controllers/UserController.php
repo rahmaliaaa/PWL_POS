0f4ca2f53d1c789896f916d6c1fc311ff4321806
rahmaliaaa <mozachikaa@gmail.com>
2025-03-14 23:20:50 +0700

    Jobsheet 4 : Praktikum 2.3 ( langkah 1 )

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,8 @@ class UserController extends Controller
         
         UserModel::create($data); */ 
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+            dd($user);
         return view('user', ['data' => $user]);
     }
 }
