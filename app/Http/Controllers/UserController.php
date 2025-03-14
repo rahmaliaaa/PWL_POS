41071c1ae305aeb16ada7d2138879eeb1ae4c393
rahmaliaaa <mozachikaa@gmail.com>
2025-03-15 00:22:59 +0700

    Jobsheet 4 : Praktikum 2.4 ( Langkah 10 )

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller
                 'level_id' => 2
             ],
         );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
