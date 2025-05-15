aae9bb6d007eafca6db7fcb24af4e7e35188e6db
rahmaliaaa <mozachikaa@gmail.com>
2025-05-15 15:05:16 +0700

    Jobsheet 7 : Praktikum 3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -11,14 +10,15 @@ class AuthorizeUser
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user();   // ambil data user yg login
-        if($user->hasRole($role)){  // fungsi user() diambil dari UserModal.php
-            return $next($request); // cek apakah user punya role yg diinginkan
+        $user_role = $request->user()->getRole();  // ambil data level_kode dari user yg login
+        if(in_array($user_role, $roles)){ // cek apakah level_kode user ada di dalam array roles
+            return $next($request); // jika ada, maka lanjutkan request
         }
+
         // jika tidak punya role, maka tampilkan error 403
         abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -14,7 +13,9 @@ class UserModel extends Authenticatable
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
     protected $hidden = ['password']; // jangan di tampilkan saat select
+
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
     /**
@@ -25,13 +26,28 @@ class UserModel extends Authenticatable
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
+    /**
+     * Mendapatkan nama role
+     */
     public function getRoleName(): string
     {
         return $this->level->level_nama;
     }
 
+    /**
+     * Cek apakah user memiliki role tertentu
+     */
     public function hasRole($role): bool
     {
         return $this->level->level_kode == $role;
     }
+
+     /**
+     * Mendapatkan nama role
+     */
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
+
 }
